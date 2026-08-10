76808f36e9cbf3b591c83d79b898f221d88df1e8
Anton Demin <56312449+ShinokuS@users.noreply.github.com>
2026-08-10 17:46:14 +0300

    feat(geometry): add full shape

---
+++ simulation/src/main/java/io/github/evoforge/simulation/world/mechanics/geometry/FullShape.java
@@ -0,0 +1,11 @@
+package io.github.evoforge.simulation.world.mechanics.geometry;
+
+public final class FullShape
+        implements Shape {
+
+    public static final FullShape INSTANCE =
+            new FullShape();
+
+    private FullShape() {
+    }
+}
