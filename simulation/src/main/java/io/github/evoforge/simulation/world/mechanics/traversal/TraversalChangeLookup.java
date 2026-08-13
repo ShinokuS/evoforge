354c4d6dde2391ba152e1434b556a27f52c1eea8
Anton Demin <56312449+ShinokuS@users.noreply.github.com>
2026-08-13 18:05:42 +0300

    feat(pathfinding): add traversal change lookup

---
+++ simulation/src/main/java/io/github/evoforge/simulation/world/mechanics/traversal/TraversalChangeLookup.java
@@ -0,0 +1,11 @@
+package io.github.evoforge.simulation.world.mechanics.traversal;
+
+/** Latest coordinated traversal mutation, used by derived caches for local invalidation. */
+public interface TraversalChangeLookup extends TraversalRevisionLookup {
+
+    int lastChangeX();
+
+    int lastChangeY();
+
+    int lastChangeZ();
+}
