695dfd1113cccbae1c8b50e05b32f925434e54ec
Anton Demin <56312449+ShinokuS@users.noreply.github.com>
2026-08-13 18:45:39 +0300

    feat(visualizer): register focused pathfinding scenarios

--- core/src/main/java/io/github/evoforge/visualizer/scenario/ScenarioCatalog.java
+++ core/src/main/java/io/github/evoforge/visualizer/scenario/ScenarioCatalog.java
@@ -36,7 +36,14 @@ public final class ScenarioCatalog {
                 new CutawayScenario(),
                 new RampNavigationScenario(),
                 new TimedMovementScenario(),
-                new OccupancyContentionScenario()));
+                new OccupancyContentionScenario(),
+                new PathfindingStraightScenario(),
+                new PathfindingStructuralDetourScenario(),
+                new PathfindingWeightedDetourScenario(),
+                new PathfindingRampScenario(),
+                new PathfindingUnreachableScenario(),
+                new PathfindingHierarchyScenario(),
+                new PathfindingInvalidationScenario()));
     }
 
     public int size() {
