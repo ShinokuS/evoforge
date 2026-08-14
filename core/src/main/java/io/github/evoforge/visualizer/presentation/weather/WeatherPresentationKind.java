33a7516805466f8512ecffd86b92a9cba1d6f7f5
Anton Demin <56312449+ShinokuS@users.noreply.github.com>
2026-08-15 02:41:43 +0300

    feat(visualizer): add weather presentation contract

---
+++ core/src/main/java/io/github/evoforge/visualizer/presentation/weather/WeatherPresentationKind.java
@@ -0,0 +1,7 @@
+package io.github.evoforge.visualizer.presentation.weather;
+
+/** Small presentation vocabulary for weather effects shown by the development visualizer. */
+public enum WeatherPresentationKind {
+    CLEAR,
+    RAIN
+}
