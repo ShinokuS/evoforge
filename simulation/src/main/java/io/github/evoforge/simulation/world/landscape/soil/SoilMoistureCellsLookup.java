209b327befd92fd65a521fa7aaec3e5276111281
Anton Demin <56312449+ShinokuS@users.noreply.github.com>
2026-08-16 00:54:30 +0300

    refactor(soil): remove legacy moisture cells lookup

--- simulation/src/main/java/io/github/evoforge/simulation/world/landscape/soil/SoilMoistureCellsLookup.java
+++
@@ -1,9 +0,0 @@
-package io.github.evoforge.simulation.world.landscape.soil;
-
-/** Read-only deterministic iteration over terrain cells retaining positive moisture. */
-public interface SoilMoistureCellsLookup {
-
-    int wetCellCount();
-
-    void forEach(SoilMoistureCellConsumer consumer);
-}
