ab9ac1fe1014e1b3ae54cee0a6b6678f0f0f7b31
Anton Demin <56312449+ShinokuS@users.noreply.github.com>
2026-08-16 17:41:23 +0300

    refactor(time): keep ProcessScheduler relative and functional

--- simulation/src/main/java/io/github/evoforge/simulation/time/ProcessScheduler.java
+++ simulation/src/main/java/io/github/evoforge/simulation/time/ProcessScheduler.java
@@ -2,10 +2,6 @@ package io.github.evoforge.simulation.time;
 
 public interface ProcessScheduler {
 
-    void scheduleAt(
-            long tick,
-            long processId);
-
     void scheduleAfter(
             long delayTicks,
             long processId);
