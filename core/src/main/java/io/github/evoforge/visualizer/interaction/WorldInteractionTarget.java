7a5aa8a4e8220c6dd8e7115f84887609ca509d50
Anton Demin <56312449+ShinokuS@users.noreply.github.com>
2026-08-17 01:36:49 +0300

    refactor: split visualizer interaction responsibilities

---
+++ core/src/main/java/io/github/evoforge/visualizer/interaction/WorldInteractionTarget.java
@@ -0,0 +1,4 @@
+package io.github.evoforge.visualizer.interaction;
+
+/** One currently visible/walkable world cell resolved from the active presentation mode. */
+record WorldInteractionTarget(int x, int y, int z) { }
