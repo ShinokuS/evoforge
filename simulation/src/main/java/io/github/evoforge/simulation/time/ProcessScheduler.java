7ce0d691b958313b63002b582351f643494fdfb3
Anton Demin <56312449+ShinokuS@users.noreply.github.com>
2026-08-16 17:37:45 +0300

    refactor(time): support absolute process wake scheduling

--- simulation/src/main/java/io/github/evoforge/simulation/time/ProcessScheduler.java
+++ simulation/src/main/java/io/github/evoforge/simulation/time/ProcessScheduler.java
@@ -2,6 +2,10 @@ package io.github.evoforge.simulation.time;
 
 public interface ProcessScheduler {
 
+    void scheduleAt(
+            long tick,
+            long processId);
+
     void scheduleAfter(
             long delayTicks,
             long processId);
