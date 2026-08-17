6244e3f439dece00616746596cc8a1f67633266e
Anton Demin <56312449+ShinokuS@users.noreply.github.com>
2026-08-17 03:15:47 +0300

    refactor(worldgen): remove duplicate hydro climate generator

--- simulation/src/main/java/io/github/evoforge/simulation/world/atlas/HydroClimateGenerator.java
+++
@@ -1,9 +0,0 @@
-package io.github.evoforge.simulation.world.atlas;
-
-import io.github.evoforge.simulation.world.genesis.WorldSpec;
-
-/** Authors long-term hydrologic climate normals from the requested world specification. */
-@FunctionalInterface
-public interface HydroClimateGenerator {
-    HydroClimateField generate(WorldSpec spec);
-}
